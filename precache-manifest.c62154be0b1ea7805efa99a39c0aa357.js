7365422e6d7e25d9727aa1a6c007c88d0adcff8d
Jcarn760 <joshuacarnahan@yahoo.com>
2023-05-03 07:21:26 -0400

    Updates

---
+++ precache-manifest.c62154be0b1ea7805efa99a39c0aa357.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8378c5e3e7e46bbde81c",
+    "url": "/ravenous/static/css/main.ee61f940.chunk.css"
+  },
+  {
+    "revision": "8378c5e3e7e46bbde81c",
+    "url": "/ravenous/static/js/main.247e6415.chunk.js"
+  },
+  {
+    "revision": "1444c97d4f0ed3607f27",
+    "url": "/ravenous/static/js/runtime~main.f8ce170b.js"
+  },
+  {
+    "revision": "eedf74e2016b73883602",
+    "url": "/ravenous/static/js/2.467af254.chunk.js"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5227795a934d9636df43c6c6cc615b41",
+    "url": "/ravenous/index.html"
+  }
+];
